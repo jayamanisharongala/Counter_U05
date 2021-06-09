a25ff82cc446cd730c13d2d3979d320645a6b0f1
jayamanisharongala <84433722+jayamanisharongala@users.noreply.github.com>
2021-06-09 04:02:11 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React, { Component } from 'react'
+import './App.css';
+import Counter from './components/Counter';
+class App extends Component {
+    render() {
+        return(
+            <div className="App">
+            <Counter/>
+            </div>
+        )
+    }
+}
+
+export default App;
